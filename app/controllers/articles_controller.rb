51b206c0c7c75946cb590f4466657083d6c818aa
Juan Garcia <juagarca@gmail.com>
2020-11-10 16:42:14 +0000

    create article and form refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,15 +7,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  # def new
-  #   @article = Article.new
-  # end
+  def new
+    @article = Article.new
+  end
 
-  # def create
-  #   @article = Article.new(article_params)
-  #   @article.save
-  #   redirect_to articles_path
-  # end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
 
   def edit
     @article = Article.find(params[:id])
